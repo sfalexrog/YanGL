0a1d47af38b47899145734fbd3bddfbc3ff25292
sfalexrog <sfalexrog@gmail.com>
2016-09-20 13:34:29 +0300

    Create a window with SDL2
    There is a simple event processing loop thrown in as well. Looks like
    you don't have to explicitly link against SDL2, as depslib handles it
    for you?

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
-#include <iostream>
-
-using namespace std;
+#include <SDL2/SDL.h>
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *w = SDL_CreateWindow("Hello OpenGL!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WindowFlags::SDL_WINDOW_OPENGL);
+
+    SDL_Event e;
+
+    bool quit = false;
+    while (!quit)
+    {
+        while(SDL_PollEvent(&e))
+        {
+            switch(e.type)
+            {
+                case SDL_EventType::SDL_QUIT:
+                    quit = true;
+                    break;
+
+            }
+        }
+    }
+    SDL_DestroyWindow(w);
     return 0;
 }
