ab3bd4440120a7ad027865f852ab6e28a1c8b0a1
sfalexrog <sfalexrog@gmail.com>
2016-09-20 14:08:43 +0300

    Add auto-generated OpenGL loader
    The loader was generated by glLoadGen and then modified to use
    SDL_GetProcAddress on glx platforms (it really should just use it
    whenever possible)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <SDL2/SDL.h>
+// Use a loader to initialize OpenGL functions; these are usually auto-generated, this one is generated through glLoadGen and manipulated to allow
+// SDL2's implementation of GetProcAddress
+#include "glload/gl_glcore_3_3.h"
+
 
 int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
+
     SDL_Window *w = SDL_CreateWindow("Hello OpenGL!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WindowFlags::SDL_WINDOW_OPENGL);
+
 
     SDL_Event e;
 
